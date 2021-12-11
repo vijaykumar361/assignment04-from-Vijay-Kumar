3b6c1a4f45f0c8c45e19dad397e8215bd5e40f8f
Vijay Kumar <95156170+vijaykumar361@users.noreply.github.com>
2021-12-11 14:46:22 +0500

    Add files via upload

---
+++ assignment04.dart
@@ -0,0 +1,96 @@
+// import 'dart:io';
+
+// void main() {
+  // print(" ");
+  // print("Task_01");
+  // print("Declare The String List:");
+  // List A = ["Name: Vijay Kumar", "Class: BSIT-Part(iii)"];
+  // print(A[0]);
+  // print(A[1]);
+
+  // print("");
+  // print("Task_02");
+  // print("Declare The Mixed List:");
+  // List M = ["Name: Vijay Kumar:", "Reg.No: 2K18-ITC-188"];
+  // print(M[0]);
+  // print(M[1]);
+
+  // print("");
+  // print("Task_03");
+  // print("Degree Program In Pakkistan:");
+  // List P = ["SSC", "HSC", "BS", "B.Com", "MS", "M.Phil", "Ph.D."];
+  // stdout.write(
+  //   P[0],
+  // );
+  // stdout.write(P[1]);
+  // stdout.write(
+  //   P[2],
+  // );
+  // stdout.write(P[3]);
+  // stdout.write(
+  //   P[4],
+  // );
+  // stdout.write(P[5]);
+  // stdout.write(
+  //   P[6],
+  // );
+
+  // print(" ");
+  // print("Task_04");
+  // print("Print Student Name, Marks, Percentage:");
+  // List list = [
+  //   "\nName: ${"Vijay"}"
+  //       "\nMarks: ${"500"}"
+  //       "\nPercentage: ${500 * 100 / 600}\n"
+  // ];
+  // print(list);
+
+  // List list1 = [
+  //   "\nName: ${"Uzair"}"
+  //       "\nMarks: ${"450"}"
+  //       "\nPercentage: ${450 * 100 / 600}\n"
+  // ];
+  // print(list1);
+
+  // List list2 = [
+  //   "\nName: ${"Tahir"}"
+  //       "\nMarks: ${"400"}"
+  //       "\nPercentage: ${400 * 100 / 600}\n"
+  // ];
+  // print(list2);
+
+//   print(" ");
+//   print("Task_05");
+//   print("Student Score:");
+//   List ss = [600, 400, 700, 650, 450, 750];
+//   print(ss);
+//   print(" ");
+//   print("Sorted Score in Acending Order:");
+//   ss.sort();
+//   print(ss);
+
+//   print(" ");
+//   print("Task_06");
+//   print("Student Score");
+//   List SS = [600, 400, 700, 650, 450, 750];
+//   print(SS);
+//   print("");
+//   print("Sorted Score in Descending Order:");
+//   var RL = new List.from(SS.reversed);
+//   print(RL);
+
+//   print("");
+//   print("Task_07");
+//   List Languages = ["Dart", "Flutter ", "Java", "PHP"];
+//   print(Languages);
+
+//   print("Enter The Subject That You Want Add:");
+//   var up = stdin.readLineSync();
+
+//   if (up == Languages) {
+//     print("Value Can't Store:");
+//   } else {
+//     Languages.replaceRange(2, 3, ["$up"]);
+//     print(Languages);
+//   }
+// }
